cc330b5dd50fa7c842a5b4819e617bd8abf37624
JerecTharen <kf7ags@gmail.com>
2020-08-27 23:45:37 -0400

    events added

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 //Initial Imports Needed
 const fs = require('fs');
 const exec = require('child_process').exec;
+const emitter = require('events');
+
+const audioFileLoaction = "D:\\Projects\\WowMandarinScript\\audio\\testForScriptAudio.mov";
+
+class Emitter extends emitter{}
+
+let taskEventEmitter = new Emitter();
 
 //List of programs that I want this script to detect
 const programList = [
@@ -18,67 +25,83 @@ let parseTasklist = (tasklist)=> {
         };
         tasks.push(task);
     }
-
     return tasks;
 };
 
-let out = exec("tasklist", (err, sdout, sderr) =>{
-    if(err)
-        //Debugging Statement
-        console.error(err);
-    if(sderr)
-        //Debugging Statement
-        console.error(sderr);
+let cmd = (cmdName) => {
+    exec(cmdName, (err, sdout, sderr) =>{
+        if(err)
+            //Debugging Statement
+            console.error(cmdName, err);
+        if(sderr)
+            //Debugging Statement
+            console.error(cmdName, sderr);
+        
+        //Debugging statement
+        console.log(cmdName, sdout);
+        taskEventEmitter.emit(cmdName, sdout);
+    });
+};
+
+cmd("tasklist");
+
+
+taskEventEmitter.on("tasklist", (out)=>{
+
+    //TODO: set up event listening remover so I can use tasklist again
     
-    //Debugging statement
-    console.log(sdout);
-    return sdout;
-});
-
-//Parse tasklist
-console.log("parsing list");
-let tasklist = parseTasklist(out);
-
-let tasksfound = [];
-programList.forEach(program => {
-    tasklist.forEach(task => {
-        if(task.taskname.includes(program))
-            tasksfound.push(
-                {
-                    task: task,
-                    index: tasklist.findIndex(()=>{
-                        return task;
-                    })
-                });
-    });
-});
-
-if(tasksfound.length > 0)
-{
-    console.log("wow running");
-    exec("start testForScriptAudio.mov", (e, o, se)=> {
-        if(e)
-        //Debugging Statement
-            console.error(e);
-        if(se)
-        //Debugging Statement
-            console.error(se);
+    //Parse tasklist
+    console.log("parsing list");
+    
+    let tasksfound = [];
+    let tasklist = parseTasklist(out);
+    programList.forEach(program => {
+        tasklist.forEach(task => {
+            if(task.taskname.includes(program))
+                tasksfound.push(
+                    {
+                        task: task,
+                        index: tasklist.findIndex(()=>{
+                            return task;
+                        })
+                    });
+        });
     });
 
-    // setTimeout(()=>{
-    //     exec(`taskkill /F /PID ${taskPID}`, (e, o, se)=> {
-    //         if(e)
-    //             //Debugging Statement
-    //             console.error(e);
-    //         if(se)
-    //             //Debugging Statement
-    //             console.error(se);
-            
-    //         //Debugging Statement
-    //         console.log("cd",o);
-    //     });
-    // }, 10000);
-}
-else{
-    console.error("not running");
-}
+    if(tasksfound.length > 0)
+    {
+        console.log("wow running");
+        // exec("start testForScriptAudio.mov", (e, o, se)=> {
+        //     if(e)
+        //     //Debugging Statement
+        //         console.error(e);
+        //     if(se)
+        //     //Debugging Statement
+        //         console.error(se);
+        // });
+
+        cmd(`start ${audioFileLoaction}`);
+
+        taskEventEmitter.on(`start ${audioFileLoaction}`, (cmdName)=>{
+            cmd("tasklist");
+        });
+
+        // setTimeout(()=>{
+        //     exec(`taskkill /F /PID ${taskPID}`, (e, o, se)=> {
+        //         if(e)
+        //             //Debugging Statement
+        //             console.error(e);
+        //         if(se)
+        //             //Debugging Statement
+        //             console.error(se);
+                
+        //         //Debugging Statement
+        //         console.log("cd",o);
+        //     });
+        // }, 10000);
+    }
+    else{
+        console.error("not running");
+    }
+});
+
